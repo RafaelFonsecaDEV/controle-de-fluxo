1bfb9b42cbaefce8998c39f328d080dc3d5eac80
rafael fonseca <rafael.fonseca2@yahoo.com.br>
2025-02-14 10:14:40 -0300

    Resultado escola

---
+++ src/ResultadoEscolar.java
@@ -0,0 +1,15 @@
+public class ResultadoEscolar {
+
+    public static void main(String[] args) {
+        int nota = 10;
+        System.out.println("nota do aluno e de:" + nota);
+        if (nota >= 7) {
+            System.out.println("aprovado");
+
+        } else {
+            System.out.println("reprovado");
+
+        }
+
+    }
+}
